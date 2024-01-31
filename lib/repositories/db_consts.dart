67a47aecb72dfc591b1757b07183bb7c4acc9b8b
Thomas-Chambers <70411859+Thomas-Chambers@users.noreply.github.com>
2024-01-31 17:36:28 -0600

    cleaned up code

--- lib/repositories/db_consts.dart
+++
@@ -1,2 +0,0 @@
-const DB_URL = "mongodb+srv://seniordesign:ZBg50hj2jIdeghp1@cluster0.atwnnol.mongodb.net/Project?retryWrites=true&w=majority";
-const USER_COLLECTION_NAME = "Users";
--- lib/view_models/auth_view_model.dart
+++ lib/view_models/auth_view_model.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:senior_design/repositories/auth_repository.dart';
 import 'package:senior_design/utils/routes/routes_name.dart';
-import 'package:senior_design/utils/utils.dart';
 
 class AuthViewModel with ChangeNotifier {
   bool _isAuthenticated = false;
@@ -14,15 +13,12 @@ class AuthViewModel with ChangeNotifier {
 
   final _authRepository = AuthRepository();
 
-  Utils _utils = Utils();
-
   Future<void> login(context, dynamic dataToBeProvided) async {
     _authRepository.login(dataToBeProvided).then((value) {
       if (value == 'success') {
         setAuthenticated(true);
         Navigator.of(context).pushNamed(RoutesName.home);
       } else{
-        _utils.showToastMessage('wrong credentials');
       }
     }).onError((error, stackTrace) {
       setAuthenticated(false);
