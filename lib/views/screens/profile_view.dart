9f06f5a02fd4d9571c131d4a8b6fa83a7f3c7070
talatcanoguz <talatcanoguz0@gmail.com>
2024-02-19 15:12:41 -0600

    fetch real data for doctors.

--- lib/views/screens/profile_view.dart
+++ lib/views/screens/profile_view.dart
@@ -42,18 +42,37 @@ class _ProfileViewState extends State<ProfileView> {
   }
 
   static Map<String, dynamic> userToMap(UserViewModel userViewModel){
-    return {
-      'First Name': userViewModel.user.firstName,
-      'Last Name': userViewModel.user.lastName,
-      'Email Address': userViewModel.user.email,
-      'Date of Birth': userViewModel.user.dateOfBirth,
-      'Gender': userViewModel.user.gender,
-      'Weight': userViewModel.user.weight,
-      'Height': userViewModel.user.height,
-      'Injury Type': userViewModel.user.injuryType,
-      'Date of Injury': userViewModel.user.dateOfInjury,
-      'Past Injuries': userViewModel.user.pastInjuries,
-      'Total Workouts': userViewModel.user.totalWorkouts
-    };
+    if(userViewModel.user.isPatient!) {
+      return {
+        'First Name': userViewModel.user.firstName,
+        'Last Name': userViewModel.user.lastName,
+        'Email Address': userViewModel.user.email,
+        'Date of Birth': userViewModel.user.dateOfBirth,
+        'Gender': userViewModel.user.gender,
+        'Weight': userViewModel.user.weight,
+        'Height': userViewModel.user.height,
+        'Injury Type': userViewModel.user.injuryType,
+        'Date of Injury': userViewModel.user.dateOfInjury,
+        'Past Injuries': userViewModel.user.pastInjuries,
+        'Total Workouts': userViewModel.user.totalWorkouts
+      };
+    }
+    else if(!userViewModel.user.isPatient!){
+      return {
+        'First Name': userViewModel.user.firstName,
+        'Last Name': userViewModel.user.lastName,
+        'Email Address': userViewModel.user.email,
+        'Hospital Name': userViewModel.user.hospitalName,
+        'Hospital Address' : userViewModel.user.hospitalAddress,
+        'Hospital City' : userViewModel.user.hospitalCity,
+        'Hospital State' : userViewModel.user.hospitalState,
+        'Hospital Zip Code' : userViewModel.user.hospitalZipCode,
+        'Hospital Floor Room' : userViewModel.user.hospitalFloorRoom,
+        'Specialization' : userViewModel.user.specialization,
+      };
+    }
+    else{
+      return <String, dynamic>{};
+    }
   }
 }
